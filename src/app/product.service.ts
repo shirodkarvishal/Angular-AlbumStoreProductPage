fb38765e945511cf608cbf3f1a9dfd130ae288e8
Vishal Shirodkar <shirodkarvishals@outlook.com>
2020-07-13 09:50:33 -0400

    fixes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,7 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productUrl = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -21,9 +21,9 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productUrl).map(
+    return this._http.get(this._productsUrl).map(
       (response) => <Product[]>response.json()
     );
-  } 
+  }
 
 }
