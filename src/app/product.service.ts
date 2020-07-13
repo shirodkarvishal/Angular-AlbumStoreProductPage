350f550a2213c833ed185f86a5f166cc62eaa43a
Vishal Shirodkar <shirodkarvishals@outlook.com>
2020-07-13 09:26:32 -0400

    list of products check-in

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,14 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -17,5 +19,11 @@ export class ProductService {
         (response) => <Album>response.json()
       );
   }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productUrl).map(
+      (response) => <Product[]>response.json()
+    );
+  } 
 
 }
